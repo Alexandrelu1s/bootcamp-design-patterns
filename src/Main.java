d143f6d66fff0c0ba821db2b7187eeadb6673ba6
alexandrelu1s <alexandrelu1s.ptn@gmail.com>
2023-10-18 14:08:43 -0300

    Singleton

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+
+public class Main {
+    public static void main(String[] args) {
+
+        Logger logger = Logger.getInstance();
+        
+        System.out.println(logger);
+        System.out.println(Logger.getInstance());
+    }
+}
